e605810b1e8c0c6114106365a7d3730e05406ba4
dhood27-a11y <hooddeja43@gmail.com>
2025-09-11 15:24:54 -0400

    rearranged rest of comments according to instructor/TA feedback

--- version-1/src/components/CountryCard.jsx
+++ version-1/src/components/CountryCard.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
 
-//created a function called CountryCard and gave it the following props based on data object provided: name, region, population, capital, and flag
-//I gave a class name of country-card for styling/structural purposes
-//I created an image tag that receives the props "name" and "flag"
-//Then a h2 that will be for country's name and passed in the prop "name"
-//Then a p tag for the countries population and passed in the prop "population" and used https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat for adding commas
-//Then a p tag for the countries region and passed in the prop "region"
-//Then a p tag for the countries capital and passed in the prop "capital" and then I used a ternary operator given each object has multiple capital key values within them so I just want to show the first capital in the array and if nothing is available then I want to display N/A
-
 function CountryCard({ name, region, population, capital, flag }) {
+  //created a function called CountryCard and gave it the following props based on data object provided: name, region, population, capital, and flag
+
   return (
+    //I embedded my Link and set the value of my prop to my dynamic URL so that when the card is clicked it accesses the country info and goes to Country Details page
+    //I gave a class name of country-card for styling/structural purposes
+    //I created an image tag that receives the props "name" and "flag"
+    //Then a h2 that will be for country's name and passed in the prop "name"
+    //Then a p tag for the countries population and passed in the prop "population" and used https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat for adding commas
+    //Then a p tag for the countries region and passed in the prop "region"
+    //Then a p tag for the countries capital and passed in the prop "capital" and then I used a ternary operator given each object has multiple capital key values within them so I just want to show the first capital in the array and if nothing is available then I want to display N/A
+
     <Link to={`/country/${name}`} className="country-link">
       <div className="country-card">
         <img
--- version-1/src/pages/SavedCountries.jsx
+++ version-1/src/pages/SavedCountries.jsx
@@ -1,13 +1,8 @@
 import { useState } from "react";
 
-//Here I created my function for Saved countries page and gave it a variable called formData and gave that a setter function I set the useState to the according info that will be needed in the form and gave each a value of an empty string given the user will provide that info when they fill it out
-//next I created handleChange function that will run each time user types or changes something on the form
-//the handleSubmit function will run when the form is submitted and useState setter function will take the initial stae and keep everything that was already there(prev) and only update the field that changed
-
-//for my actual form I created a form element and passed in my handleSubmit which onSubmit will call once the form is actually submitted
-//for each input the type is equal to the type of input the user will be doing, the name identifies the type of input needed, the placeholder is what will be in the input field to let the user know what they are typing, each input has a value prop that is connected to the formData which holds all the values of the form fields, onChange calls handleChange so typing is updated/tracked by each keystroke
-//lastly I gave it a submit button that will store info once form is submitted
 function SavedCountries() {
+  //Here I created my function for Saved countries page and gave it a variable called formData and gave that a setter function I set the useState to the according info that will be needed in the form and gave each a value of an empty string given the user will provide that info when they fill it out
+
   const [formData, setFormData] = useState({
     fullName: "",
     email: "",
@@ -15,6 +10,8 @@ function SavedCountries() {
     bio: "",
   });
 
+  //next I created handleChange function that will run each time user types or changes something on the form
+  //the handleSubmit function will run when the form is submitted and useState setter function will take the initial state and keep everything that was already there(prev) and only update the field that changed
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
@@ -30,6 +27,10 @@ function SavedCountries() {
 
   return (
     <>
+      {/* //for my actual form I created a form element and passed in my handleSubmit which onSubmit will call once the form is actually submitted
+//for each input the type is equal to the type of input the user will be doing, the name identifies the type of input needed, the placeholder is what will be in the input field to let the user know what they are typing, each input has a value prop that is connected to the formData which holds all the values of the form fields, onChange calls handleChange so typing is updated/tracked by each keystroke
+//lastly I gave it a submit button that will store info once form is submitted */}
+
       <div className="saved-countries-form">
         <h2>My Saved Countries</h2>
         <h2>My Profile</h2>
