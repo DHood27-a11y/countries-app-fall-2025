c27c41767d3999479eccf0412663e1e5ee5d3c9a
dhood27-a11y <hooddeja43@gmail.com>
2025-09-25 16:46:03 -0400

    added POST and GET to form data, stored new user

---
+++ version-3/src/pages/CountryDetail.jsx
@@ -0,0 +1,150 @@
+import { useNavigate, useParams } from "react-router-dom";
+import "../CountryDetail.css";
+import { useState, useEffect } from "react";
+
+//for back button: https://stackoverflow.com/questions/52039083/handle-back-button-with-react-router
+
+function CountryDetail({ countriesData }) {
+  /* ------------- Country View Count(not finished) -------------- */
+  // created a useState to track how many times a country has been viewed and set initial state to 0 since we will be going up by 1 each time
+  const [viewCount, setViewCount] = useState(0);
+
+  //I want this function to run once when the card is rendered
+  useEffect(() => {
+    //will be getting the view counts from localStorage and if it doesnt exist we will start with an empty object. JSON.parse will convert string back into original data type
+    const savedViews = JSON.parse(localStorage.getItem("countryViews")) || {};
+
+    //I want to make sure the count for each country is current and default it to 0
+    const count = savedViews[name] || 0;
+
+    // I want to make the count increase by one which will ultimately update the state
+    setViewCount(count + 1);
+
+    //Created a new object that will have updated count for selected country
+    const updatedViews = { ...savedViews, [name]: count + 1 };
+
+    //now I want to save my updated view count to the localStorage by using .setItem and .stringify method
+    localStorage.setItem("countryViews", JSON.stringify(updatedViews));
+  }, []);
+
+  //declared function called CountryDetail and gave it a parameter of countriesData given that is what contains data object
+  const { countryName } = useParams();
+  //this function pulls the countryName value from the API fetch URL
+  const navigate = useNavigate();
+
+  /* ------- FIRST FUNCTION: selectedCountry ------------*/
+  //this function helps the user be able to navigate to a different page
+  function selectedCountry(country) {
+    //this function just checks to make sure a country's name matches the one in the URL and returns the result
+    return country.name.common === countryName;
+  }
+  //this function uses the .find method to find the first country that matches
+  const country = countriesData.find(selectedCountry);
+
+  if (!country) return <p>Country not found</p>;
+  //the ternary statement just says if no country was found because the name didnt match show the following message, otherwise just show the country's detail page
+
+  /* --------- SECOND FUNCTION: handleSave ------------- */
+  //this function will save the country to localStorage when user clicks the save button
+  const handleSave = () => {
+    let savedCountries;
+
+    try {
+      //the try catch method will get savedCountries from localStorage and convert string to array using .parse method and if nothing is there then it will be an empty array
+      savedCountries = JSON.parse(localStorage.getItem("savedCountries")) || [];
+    } catch {
+      savedCountries = [];
+    }
+
+    //this will track if a country is saved already or not
+    let alreadySaved = false;
+
+    /*  ---------- LOOPING AND IF ELSE STATEMENT ----------- */
+    //Looping through each saved country to check for duplicates
+    for (let i = 0; i < savedCountries.length; i++) {
+      //check if the current saved country matches the country the user wanted saved
+      if (savedCountries[i].name.common === country.name.common) {
+        //If the country matches mark it as already saved
+        alreadySaved = true;
+        //if its not a match keep alreadySaved as false or do nothing
+      }
+    }
+    //using bang operator to check if the country has NOT been saved already
+    if (!alreadySaved) {
+      //If the country is not in the saved list then add it using .push method
+      savedCountries.push(country);
+      //then I want to update the localStorage with the new savedCountries array
+      localStorage.setItem("savedCountries", JSON.stringify(savedCountries));
+      //then I want to notify the user that the country has been added to saved list by using alert function with a template literal to display message to user
+      alert(`${country.name.common} saved!`);
+    } else {
+      //if the country has already been saved I also want to notify the user so they will know
+      alert(`${country.name.common} is already saved.`);
+    }
+  };
+
+  return (
+    <>
+      {/* ---------- STRUCTURE FOR COUNTRY DETAIL PAGE ------------ */}
+
+      <div className="country-detail-container">
+        {/* //Created the container div for styling as well as structural purposes 
+    // The back button when clicked(onClick) should take the user back to the prev page using the navigate function */}
+        <button className="back-button" onClick={() => navigate(-1)}>
+          {" "}
+          ← Back{" "}
+        </button>
+        {/* // Created the div for "country-detail-main" to styling and structural */}
+        <div className="country-detail-main">
+          {/* used img tag for picture of flag gave the following attributes: src-where the image file is from, alt-the description of the flag in case it doesnt show, className-for styling and structural purposes */}
+          <img
+            src={country.flags.png}
+            alt={`Flag of ${country.name.common}`}
+            className="country-flag"
+            style={{
+              width: "650px",
+              height: "auto",
+              maxWidth: "100%",
+              border: "1px solid #ddd",
+              borderRadius: "8px",
+              objectFit: "contain",
+              display: "block",
+            }}
+          />
+          {/* created div "country-info" for structural and styling purposes */}
+          <div className="country-info">
+            {/* used an h1 element to display the name of the country and passed in country.name.common from the data object */}
+            <h1 className="country-name">{country.name.common}</h1>
+            {/* given the UI design provided next I created a save button */}
+            <button className="save-button" onClick={handleSave}>
+              Save
+            </button>
+            {/* next I created a p tag that will be for the population and passed in country.population from the data object and .toLocaleString() is so that the commas are included */}
+            <p>
+              <strong>Population:</strong> {country.population.toLocaleString()}
+            </p>
+            {/* created another p tag for the region and passed in country.region from data object */}
+            <p>
+              <strong> Region: </strong>
+              {country.region}
+            </p>
+            {/* created a p tag and passed country.capital from data object and put ternary statement to say if the country exists display the first one and if not show the message "N/A" */}
+            <p>
+              <strong>Capital:</strong>{" "}
+              <span>{country.capital?.[0] || "N/A"}</span>
+            </p>
+            <div className="view-count">
+              <h3>{name}</h3>
+              <p>
+                <strong>Viewed: </strong>
+                {viewCount} times
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CountryDetail;
---
+++ version-3/src/pages/SavedCountries.jsx
@@ -0,0 +1,192 @@
+import { useEffect, useState } from "react";
+import CountryCard from "../components/CountryCard";
+
+function SavedCountries() {
+  //Here I created my Saved countries page and created and empty form state (all fields will be blank until user types)
+
+  const emptyFormState = {
+    fullName: "",
+    email: "",
+    country: "",
+    bio: "",
+  };
+
+  // ----------- UseState --------------------
+
+  //formData will hold the info the user is typing in the form
+  const [formData, setFormData] = useState(emptyFormState);
+
+  //userInfo will hold the saved profile info after the form is submitted
+  const [userInfo, setUserInfo] = useState(null);
+
+  //savedCountries will hold the list of countries that user chose to save
+  const [savedCountries, setSavedCountries] = useState([]);
+
+  //------------ Handle Change ------------------------
+
+  //next I created handleChange function that will run each time user types or changes something on the form
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  //---------- ASYNC AND FETCH  ----------------------
+  const getNewestUser = async () => {
+    //declare a variable that will hold the response from the GET request to /get-newest-user
+    try {
+      const response = await fetch(
+        "https://backend-answer-keys.onrender.com/get-newest-user"
+      );
+      //turn the response into json format
+      const data = await response.json();
+      console.log(data);
+      const newestUserFromApi = data[0];
+      //save the data in state
+      setUserInfo({
+        fullName: newestUserFromApi.name,
+        email: newestUserFromApi.email,
+        country: newestUserFromApi.country,
+        bio: newestUserFromApi.bio,
+      });
+    } catch (err) {
+      console.error("Couldnt get new user", error);
+    }
+  };
+
+  //when we call the fetch() function, we only need to pass in
+  //the API URL as one parameter when its a GET request
+  //but we need to make a POST request, we have to pass in a second parameter: an OBJECT
+  const storeUserData = async () => {
+    const response = await fetch(
+      "https://backend-answer-keys.onrender.com/add-one-user",
+      {
+        method: "POST",
+        headers: {
+          //the headers is where we put metadata about our request, including the data type that we pass in the body
+          //in this case, we are saying we're passing in JSON data in the body
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: formData.fullName,
+          country_name: formData.country,
+          email: formData.email,
+          bio: formData.bio,
+        }),
+      }
+    );
+  };
+
+  //we arent gonna do anything with
+
+  //------------- Handle Submit ------------------
+  /*
+  the handleSubmit function will run when the form is submitted and useState setter
+   function will take the initial state and keep everything that was already there
+   (prev) and only update the field that changed
+  */
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("data:", formData);
+
+    //store the users profile data by sending a POST request to the API
+    storeUserData();
+
+    //then updating the userInfo state with formData so that the welcome message will show
+    setUserInfo(formData);
+
+    //then I want to reset the form back to empty so that when page refreshes it can be filled out again potentially by someone else
+    setFormData(emptyFormState);
+  };
+
+  //------------- USE EFFECT -------------------
+
+  useEffect(() => {
+    getNewestUser();
+  }, []);
+
+  //I want my useEffect run once when the page loads so I will give it an empty dependency array
+  useEffect(() => {
+    //gets the saved countries from localStorage
+    const saved = localStorage.getItem("savedCountries");
+    //Parses string into array and will go into setter function as a new array
+    if (saved) {
+      const countries = JSON.parse(saved);
+      setSavedCountries(countries);
+    }
+  }, []);
+
+  //------------- Return/Rendering ------------------------
+
+  return (
+    <>
+      {/* ---------- FORM --------------------
+      
+      
+//for each input the type is equal to the type of input the user will be doing,
+//  the name identifies the type of input needed, the placeholder is what will be in the input field to let the user know what they are typing, 
+// each input has a value prop that is connected to the formData which holds all the values of the form fields,
+//  onChange calls handleChange so typing is updated/tracked by each keystroke
+//lastly I gave it a submit button that will store info once form is submitted */}
+
+      {/* I want to render this message only if userInfo exists*/}
+      {userInfo && <h2>Welcome {userInfo.fullName}</h2>}
+      <div className="saved-countries-form">
+        <h2>My Saved Countries</h2>
+        {/* ---------- MAPPING AND EMBEDDING -------------- */}
+        <div className="country-list">
+          {/* I imported Country card given I want to reuse the same card design/component that I made for the Home page.
+        This way my saved countries will display using the same layout.  */}
+
+          {/* This will show a message if no countries are saved */}
+          {savedCountries.length === 0 && <p>No countries saved. </p>}
+          {/* Mapped over savedCountries array and embedded Country card */}
+          {savedCountries.map((country, index) => (
+            <CountryCard key={index} country={country} />
+          ))}
+        </div>
+        <h2>My Profile</h2>
+        {/* //for my actual form I created a form element and passed in my handleSubmit which onSubmit will call once the form is actually submitted */}
+        <form className="profile-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="fullName"
+            placeholder="Full Name"
+            value={formData.fullName}
+            onChange={handleChange}
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+
+          <input
+            type="text"
+            name="country"
+            placeholder="Country"
+            value={formData.country}
+            onChange={handleChange}
+          />
+
+          <textarea
+            name="bio"
+            placeholder="Bio"
+            value={formData.bio}
+            onChange={handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default SavedCountries;
