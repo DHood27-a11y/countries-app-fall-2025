77ef2ff39144391c21af4c63743c669aa91af085
Deja <hooddeja43@gmail.com>
2025-11-13 13:01:39 -0500

    Delete version-4/server/src/config.js

--- version-4/server/src/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  databaseUrl:
-    "postgresql://neondb_owner:npg_zPLMJH2dOKT7@ep-small-shape-aemmkewh-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-};
-
-export default config;
