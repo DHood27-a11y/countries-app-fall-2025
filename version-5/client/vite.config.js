73a905b55a6b7c818c0261f24785fae02f03b1cf
dhood27-a11y <hooddeja43@gmail.com>
2025-12-11 15:20:36 -0500

    corrected vite.config.js path

--- version-5/client/vite.config.js
+++ version-5/client/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://countries-app-fall-2025-qvo9.onrender.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, ""),
